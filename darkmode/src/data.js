379e0d9d56ae8acf6455b5e55d58cca9b3984871
Atharva Chandwadkar <chandwadkar28@gmail.com>
2021-05-18 19:32:30 +0530

    Added Darkmode sample

---
+++ darkmode/src/data.js
@@ -0,0 +1,32 @@
+const articles = [
+  {
+    id: 1,
+    title: 'Dr K K Aggarwal loses battle with Covid-19',
+    date: new Date(2021, 5, 17),
+    length: 11,
+    snippet: `The 62-year-old was admitted to AIIMS for the past several days and was on ventilator support since last week`,
+  },
+  {
+    id: 2,
+    title: 'J&K Govt Orders Posting Of KAS Officer',
+    date: new Date(2019, 10, 22),
+    length: 5,
+    snippet: `JAMMU, May 18: J&K Government ordered posting of KAS Officer.`,
+  },
+  {
+    id: 3,
+    title: 'Lockdown in Odisha extended till June 1 - here is whats allowed and whats not',
+    date: new Date(2018, 7, 11),
+    length: 5,
+    snippet: `Odisha Chief Minister Naveen Patnaik on Monday announced that the government will conduct a house-to-house survey for three months to record COVID-19 symptoms in villagers.`,
+  },
+  {
+    id: 4,
+    title: 'Mumbai News Live: BMC extends deadline for Covid vaccine global tender',
+    date: new Date(2015, 5, 4),
+    length: 5,
+    snippet: `Gusty winds along with heavy rains lashed Mumbai and its neighbouring areas on Monday as the 'very severe cyclonic storm' Tauktae passed close to the coast while moving towards Gujarat. Several trees`,
+  },
+]
+
+export default articles
