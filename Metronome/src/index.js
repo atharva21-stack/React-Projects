3e14adf8238b38dd6f0fb437e8b72b95d0225f3b
Atharva Chandwadkar <70438130+atharva21-stack@users.noreply.github.com>
2022-06-02 06:15:32 -0700

    Delete Metronome directory

--- Metronome/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import Metronome from './Metronome';
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<Metronome/>, document.getElementById('root'));
-registerServiceWorker();
-
